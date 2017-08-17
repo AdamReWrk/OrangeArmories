998c4bf17a78aa405429a0b7164af29153a4c8ed
AdamReWrk <adam@rewrk.co.uk>
2017-08-17 11:31:42 +0100

    Chnages

--- JS/functions.js
+++ JS/functions.js
@@ -1,5 +1,5 @@
 /*-------------- On Scroll Functions ----------------*/
-$(window).scroll(function(){    
+$(window).scroll(function(){
     var scroll = $(window).scrollTop();
     if (scroll >= 50) {
         $("#header, #logo, #toggle, .toplevel, #megamenu, #shopingcartli").addClass("scrolled");
@@ -22,4 +22,12 @@ $(document).ready(function(){
 /*-------------- Calling Owl Carosel ----------------*/
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel();
+});
+
+
+/*-------------- Paralax Banner ------------------------*/
+$(window).scroll(function() {
+    var y = $(this).scrollTop(); // Pos
+    var r = -3; // Rate
+    $('#banner').css('background-position', 'center ' + parseInt(y/r) + 'px'); // Switch x polarity to reverse direction
 });
